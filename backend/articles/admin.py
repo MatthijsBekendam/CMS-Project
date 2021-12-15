a95e7c94f8037689603dc0d1cde2c05e0780ae47
Matthijs Bekendam <matthijs.bekendam@luminext.eu>
2021-12-15 18:35:02 +0100

    very basic django back-end setup

--- backend/articles/admin.py
+++ backend/articles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description')
+
 
 # Register your models here.
+
+admin.site.register(Article, ArticleAdmin)
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from articles import views
+router = routers.DefaultRouter()
+router.register(r'articles', views.ArticleView, 'articles')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
