930b198c67075f0f337e426ff197f25ec1894df4
Matthijs Bekendam <matthijs.bekendam@luminext.eu>
2021-12-16 16:35:05 +0100

    rename project

--- alteza_django_project/backend/urls.py
+++ alteza_django_project/backend/urls.py
@@ -1,4 +1,4 @@
-"""backend URL Configuration
+"""alteza_django_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
